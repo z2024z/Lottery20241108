9f57ec049b6e72e0326f2d06d3842f62d6e2745e
z2024z <unity2024z@outlook.com>
2024-11-08 13:06:35 +0800

    zho

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 import pandas as pd
-
 data = pd.read_json('lotteryData\ssq20241108.json')
-print(data)
+# df = pd.DataFrame(data)
+# print(df)
+# print(data.info())
+# a = [1,2,3]
+# myVar01 = pd.Series(a)
+# print(myVar01)
+# print(data)
 # print('loft')
+
